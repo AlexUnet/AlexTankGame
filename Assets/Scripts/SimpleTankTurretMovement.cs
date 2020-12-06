d5645653bd68b5e314feeaa0a330c3c3b80272a4
AlexUnet <alejandro.lopezp@unet.edu.ve>
2020-12-05 23:32:32 -0400

    First commit

---
+++ Assets/Scripts/SimpleTankTurretMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTankTurretMovement : MonoBehaviour
+{
+    public Transform TurretY;
+    public float range = 1000f;
+
+// Update is called once per frame
+    void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit, range);
+
+        Vector3 dir = hit.point - transform.position;        
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = lookRotation.eulerAngles;
+            TurretY.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+}
---
+++ Assets/Scripts/TankFireController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankFireController : MonoBehaviour
+{
+
+    public Transform cannon;
+    public Animator fire_A;
+    GameObject shell;
+    public ParticleSystem fireEffect;
+    bool fire;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){   
+            Instantiate(fireEffect,transform.position,transform.rotation);
+            fire_A.SetBool("fire",true);
+            Debug.Log("FIRE");
+            //Instantiate(shell,transform.position,transform.rotation);
+            StartCoroutine(Reload());            
+        }        
+    }
+
+    IEnumerator Reload(){
+        fire = !fire;
+        yield return new WaitForSeconds(5);
+        
+    }
+}
