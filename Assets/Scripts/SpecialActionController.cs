a2aba186946be578ff8de86120067cd6945b8b0b
AlexUnet <alejandro.lopezp@unet.edu.ve>
2020-12-20 03:04:27 -0400

    sistema de mira con el canvas listo, se cambian entre camaras y todo en el display 1(considerar mejorar el sistema de mira que no vaya al punto sino a unos cuanots metros frente a el rayo de la pantalla) se bugeaba porque el tanque de prueba también tenía una camara

---
+++ Assets/Scripts/SpecialActionController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialActionController : MonoBehaviour
+{
+    [SerializeField]Camera thirdPersonCamera;
+    [SerializeField]Camera firstPersonCamera;
+    [SerializeField]Canvas scopeCanvas;
+    
+    int current; int second;
+    bool state;
+    bool zoom;
+
+    void Awake(){
+
+    }
+    void LateUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            if(!state){
+                state = true;
+                current = 1; second = 0;
+            }else{
+                current = 0; second = 1;
+                state = false;
+            }
+            scopeCanvas.enabled = state;
+            thirdPersonCamera.targetDisplay = current;
+            firstPersonCamera.targetDisplay = second;
+        }
+
+        if(Input.GetMouseButtonDown(1)){
+            if(!zoom){
+                zoom = true;
+                firstPersonCamera.fieldOfView = 17;
+            }else{
+                zoom = false;
+                firstPersonCamera.fieldOfView = 27;
+            }
+            
+        }
+    }
+}
